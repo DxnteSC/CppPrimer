7e83301e21b09e0826b6cfd3d3dba5744d1884bd
Dante Spencer-Campbell <dantes_c@outlook.com>
2018-12-29 22:38:58 +0000

    Completed upto Section 1.2

---
+++ Ch1/Ex1.3.cpp
@@ -0,0 +1,10 @@
+//program to print Hello World
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World" << std::endl;
+    return 0;
+}
+
---
+++ Ch1/Ex1.4.cpp
@@ -0,0 +1,12 @@
+//read user input and prints the product
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter two numbers" << std::endl;
+    int v1 = 0, v2 = 0;
+    std::cin >> v1 >> v2;
+    std::cout << "The product of " << v1 << " and " << v2 << " is " << v1 * v2 << std::endl;
+    return 0;
+}
