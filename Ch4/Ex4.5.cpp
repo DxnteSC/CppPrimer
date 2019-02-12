b52019246cd2c4438f4398804d9788ca41018ade
Dante Spencer-Campbell <dantes_c@outlook.com>
2019-02-12 22:45:02 +0000

    Finished upto 4.3

---
+++ Ch4/Ex4.5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // (a) -30 * 3 + 21 / 5 = -86
+    // (b) -30 + 3 * 21 / 5 = -18
+    // (c) 30 / 3 * 21 % 5 = 0
+    // (d) -30 / 3 * 21 % 4 = -2
+    
+    cout << -30 * 3 + 21 / 5 << endl;
+    cout << -30 + 3 * 21 / 5 << endl;
+    cout << 30 / 3 * 21 % 5  << endl;
+    cout << -30 / 3 * 21 % 4 << endl;
+    
+}
---
+++ Ch4/Ex4.6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    cin >> i;
+    
+    if (i % 2 == 0)
+    {
+        cout << "i is even" << endl;
+    }
+    else
+    {
+        cout << "i is odd" << endl;
+    }
+}
