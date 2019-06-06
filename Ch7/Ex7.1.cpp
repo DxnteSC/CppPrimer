6acdc5e8e3bf0fd5328ac30da6232acb75faba4f
Dante Spencer-Campbell <dantes_c@outlook.com>
2019-06-06 13:38:25 +0100

    Finished upto 7.1.2

---
+++ Ch7/Ex7.1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+struct Sales_data
+{
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+
+int main() {
+    
+    Sales_data total;
+    double price;
+    if (std::cin >> total.bookNo >> total.units_sold >> price) {
+        total.revenue = total.units_sold * price;
+        Sales_data trans;
+        while (std::cin >> trans.bookNo >> trans.units_sold >> price) {
+            trans.revenue = trans.units_sold * price;
+            if (trans.bookNo == total.bookNo) {
+                total.units_sold += trans.units_sold;
+                total.revenue += trans.revenue;
+            } else {
+                std::cout << "Sales for: " << total.bookNo << std::endl;
+                std::cout << "Units sold: " << total.units_sold << std::endl;
+                std::cout << "Total Revenue " << total.revenue << std::endl;
+                total = trans;
+            }
+        }
+        std::cout << "Sales for: " << total.bookNo << std::endl;
+        std::cout << "Units sold: " << total.units_sold << std::endl;
+        std::cout << "Total Revenue " << total.revenue << std::endl;
+    } else {
+        std::cerr << "No Data" << std::endl;
+        return -1;
+    }
+    return 0;
+}
