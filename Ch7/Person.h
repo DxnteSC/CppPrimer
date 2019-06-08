00fcd1a9a1f31f3e682c94f4e0d89b83ef217a6c
Dante Spencer-Campbell <dantes_c@outlook.com>
2019-06-08 06:37:13 +0100

    Finished upto 7.3

--- Ch7/Person.h
+++ Ch7/Person.h
@@ -4,18 +4,23 @@
 #include <string>
 #include <iostream>
 
-struct Person
+class Person
 {
-    std::string name;
-    std::string address;
     
+    friend std::istream &read(std::istream&, Person&);
+    friend std::ostream &print (std::ostream&, const Person&);
+
+public:
     std::string getName() const {return name;} //const as should be able to read but not write
     std::string getAddress() const {return address;} //const as should be able to read but not write
-    
     Person() = default;
     Person(const std::string& n) : name(n), address("") {};
     Person(const std::string& n, const std::string& a) : name(n), address(a) {}
     Person(std::istream&);
+
+private:
+    std::string name;
+    std::string address;
 };
 
 std::istream &read(std::istream&, Person&);
--- Ch7/Sales_data.h
+++ Ch7/Sales_data.h
@@ -4,15 +4,26 @@
 #include <string>
 #include <iostream>
 
-struct Sales_data
+class Sales_data
 {
+friend std::istream &read (std::istream&, Sales_data&);
+    
+friend std::ostream &print (std::ostream&, const Sales_data&);
+    
+friend Sales_data add(const Sales_data &lhs, const Sales_data &rhs);
+
+public:
     Sales_data() = default;
     Sales_data(const std::string& s) : bookNo(s) {}
     Sales_data(const std::string& s, unsigned n, double p) : bookNo(s), units_sold(n), revenue(n*p) {}
     Sales_data(std::istream&);
     std::string isbn() const {return bookNo;}
     Sales_data& combine(const Sales_data&);
-    double avg_price() const;
+
+private:
+    double avg_price() const {
+        return units_sold ? revenue/units_sold : 0;
+    }
     std::string bookNo;
     unsigned units_sold = 0;
     double revenue = 0.0;
