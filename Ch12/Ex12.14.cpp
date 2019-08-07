132851e70e350498e8e0d2d2443301370aa465ac
Dante Spencer-Campbell <dantes_c@outlook.com>
2019-08-07 21:26:34 +0100

    Finished upto 12.1.5

---
+++ Ch12/Ex12.14.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+struct destination
+{
+    std::string destinationName;
+    unsigned destinationID;
+    destination(const std::string& s = " ", unsigned i = 0): destinationName(s), destinationID(i) {}
+};
+
+struct connection
+{
+    destination* connectionDest;
+    int port;
+};
+
+connection connect(destination* d, int i)
+{
+    connection c;
+    c.connectionDest = d;
+    c.port = i;
+    std::cout << "Setting up new connection to " << c.connectionDest->destinationName << " using ID code " << c.connectionDest->destinationID << " at port " << c.port << "." << std::endl;
+    return c;
+}
+
+void disconnect(connection c)
+{
+    std::cout << "Disconnecting from " << c.connectionDest->destinationName << std::endl;
+    c.connectionDest = nullptr;
+}
+
+void endConnection(connection* c)
+{
+    disconnect(*c);
+}
+
+void manageConnection(destination& d)
+{
+    connection c = connect(&d,5);
+    std::shared_ptr<connection> p(&c, endConnection);
+    std::cout << "Managing connection of " << p->connectionDest->destinationName << std::endl;
+}
+
+int main()
+{
+    destination d("Test", 555);
+    manageConnection(d);
+}
+
